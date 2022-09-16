69bcdaa7caef371a1f6c2b7afc8b5c68c1e7f82a
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-09-16 16:35:09 +0200

    js tasks
    completed task number 4

---
+++ src/js/task-04.js
@@ -0,0 +1,18 @@
+const $spanValue=document.getElementById("value");
+const $decrementBtn=document.querySelector('button[data-action="decrement"]');
+const $incrementBtn=document.querySelector('button[data-action="increment"]');
+
+let counterValue=0;
+
+function decrementValue(){
+  counterValue-=1;
+  $spanValue.textContent=counterValue;
+};
+
+function incrementValue(){
+  counterValue+=1;
+  $spanValue.textContent=counterValue;
+};
+
+$decrementBtn.addEventListener("click", decrementValue);
+$incrementBtn.addEventListener("click", incrementValue);
