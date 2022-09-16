41b8c6f5f2a15b8adcf756523110cc4aea2b13d8
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-09-16 21:28:46 +0200

    task js
    completed task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+const $input=document.getElementById("name-input");
+const $spanResult=document.getElementById("name-output");
+
+function insertNameToSpan() {
+  if (!$input.value) {
+    $spanResult.textContent="Anonymous";
+    return
+  };
+  $spanResult.textContent=$input.value
+};
+
+$input.addEventListener("input", insertNameToSpan);
