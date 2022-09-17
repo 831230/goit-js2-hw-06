7b090249290c519872efd8343358605b7658076d
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-09-17 15:29:50 +0200

    js tasks
    completed task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const $input=document.querySelector('[data-length="6"]');
+
+const inputValueLength=$input.dataset.length;
+
+function checkLongValueInput() {
+  if ($input.value.length==inputValueLength) {
+    $input.classList.remove("invalid");
+    $input.classList.add("valid");
+    return
+  }
+  $input.classList.remove("valid");
+  $input.classList.add("invalid")
+}
+
+$input.addEventListener("blur",checkLongValueInput);
