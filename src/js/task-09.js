8e581f691c026adcd5fe96c6553f9e912e86d9c6
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-09-17 19:08:27 +0200

    js tasks
    completed task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const $colorValueSpan=document.querySelector(".color");
+const $changeColorBtn=document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+function changeColor(){
+  const currentColor=getRandomHexColor();
+  document.querySelector("body").style.backgroundColor=currentColor;
+  $colorValueSpan.textContent=currentColor;
+};
+
+$changeColorBtn.addEventListener("click",changeColor);
